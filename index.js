4299bf9294ce183caf24092e274c19b4f5107843
Haven <415122975@qq.com>
2018-04-16 16:44:08 +0800

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ const ScrollableTabView = createReactClass({
     onChangeTab: PropTypes.func,
     onScroll: PropTypes.func,
     renderTabBar: PropTypes.any,
-    style: View.propTypes.style,
+    style: ViewPropTypes.style,
     contentProps: PropTypes.object,
     scrollWithoutAnimation: PropTypes.bool,
     locked: PropTypes.bool,
